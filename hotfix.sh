dba2bf52dd8608b086f85ce26e4bbda13253b12e
Rhoy DO <syourhoy.do@gmail.com>
2019-10-22 17:25:23 +0200

    a

--- hotfix.sh
+++ hotfix.sh
@@ -7,7 +7,7 @@ usage() {
 }
 
 getPackageVersion() {
-  PACKAGE_VERSION=$(cat ../package.json \
+  PACKAGE_VERSION=$(cat ./package.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
